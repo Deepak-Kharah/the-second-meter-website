e996f831a31e52c659d6cce7ccb26ab93725d712
deepak_kharah <kharahdeepak009@gmail.com>
2019-10-21 20:55:37 +0530

    rest framework initialize

---
+++ src/tsm_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TsmApiConfig(AppConfig):
+    name = 'tsm_api'
--- src/user/forms.py
+++ src/user/forms.py
@@ -6,9 +6,3 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
-
-        widgets = {
-            'username': forms
-        }
-
-
